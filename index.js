7682cc45ad52e560123dad8f1ad79c11957a43f7
MoazAbdullah1 <moazabdullah02@gmail.com>
2023-08-27 00:30:25 +0500

    index.js file

---
+++ index.js
@@ -0,0 +1,51 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+};
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
+
+    if (passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8) {
+        setError(password, 'Password must be at least 8 characters');
+    } else {
+        setSuccess(password);
+        // If all validations pass, you can proceed with the login logic here
+        // For this example, let's just log a success message
+        console.log('Login successful!');
+    }
+};
+
+
